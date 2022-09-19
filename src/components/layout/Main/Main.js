b05a4fa64cfb0632ed0de1674912a2982b22505e
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-19 14:00:59 -0700

    useBlogs is now a custom hook all tests pasing

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import BlogCard from '../../BlogCard/BlogCard';
-import { getBlogs } from '../../../services/getBlogs';
+import useBlogs from '../../../hooks/useBlogs';
 
 export default function Main() {
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getBlogs();
-      setBlogs(data);
-    }
-    fetchData();
-
-    //empty array only runs on load, nothing runs with every change, defining something runs only with the change of the defined
-  }, []);
-
   return (
     <main>
-      {blogs.map((blog) => (
+      {useBlogs().map((blog) => (
         <BlogCard key={blog.id} {...blog} />
       ))}
     </main>
