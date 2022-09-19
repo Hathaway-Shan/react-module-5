51721b180b20cbe3c428a963ab7f701a2b54f00d
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-19 14:54:52 -0700

    added foriegn key authors to supabase call authors name now displays on display

--- src/services/getBlogs.js
+++ src/services/getBlogs.js
@@ -1,6 +1,13 @@
 import { checkError, client } from './client';
 
+//added additional syntax for calling authors from foreign key table
 export async function getBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select(`
+  *,
+  authors (
+    name
+  )
+  `);
+
   return checkError(resp);
 }
